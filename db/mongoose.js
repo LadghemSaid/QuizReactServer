21c99d6f4104317680b82a9f27a4baaa91aec9b5
totor <toto@toot.fr>
2019-11-09 14:41:42 +0100

    first commit serveur

---
+++ db/mongoose.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// db connexion
+mongoose.connect('mongodb://localhost/quizz',{ useNewUrlParser: true });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log("connected to", db.client.s.url);
+});
+
+// database collections
+const usersSchema = mongoose.Schema({
+  name: String,
+  password: String
+});
+
+const quizzesSchema = mongoose.Schema({
+  name: String,
+  icon: String,
+  keywords: [],
+  questions: [],
+  published: Boolean,
+  ownerId: Schema.Types.ObjectId,
+  scores: []
+});
+
+
+// exports
+const Users = mongoose.model('Users', usersSchema);
+const Quizzes = mongoose.model('Quizzes', quizzesSchema);
+
+module.exports = {};
+module.exports.users = Users;
+module.exports.quizz = Quizzes;
+
---
+++ router.js
@@ -0,0 +1,52 @@
+const express = require("express");
+
+const bodyParser = require("body-parser");
+const router = express.Router();
+
+const db = require('./db/mongoose.js');
+const Users = db.users;
+const Quizz = db.quizz;
+
+router
+  .use(express.static('resources'))
+  .use(bodyParser.json()) // for parsing application/json
+  .use(bodyParser.urlencoded({
+    extended: true
+  })) // for parsing application/x-www-form-urlencoded
+    .get('/quizz', (req, res) => {
+    Quizz.find({})
+        .exec((err, data) => {
+            if (err)
+                return res.status(500).send(err);
+            else
+                //console.log("quizz/", data);
+
+            res.json(data);
+        })
+}).get("/getquizz/:id", (req, res) => {
+    console.log("Dans le router");
+        let uid = parseInt(req.params.id);
+    //console.log(uid);
+        Quizz.findOne(
+            {
+                _uid: uid
+               // _uid: 1
+            }
+        ).exec((err, data) => {
+      // console.log(data,err);
+        if (err)
+            return res.status(500).send(err);
+        else
+            res.json(data);
+          //  console.log("server",data);
+    })
+})
+    .use((req, res) => {
+    res.status(400);
+    res.json({
+      error: "Bad request"
+    });
+  });
+
+
+module.exports = router;
